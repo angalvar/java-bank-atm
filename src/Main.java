cd7ab91bdb679a463b94d1752bf8dbb7295662c0
Angel Alvarado <angalvar@hotmail.com>
2024-10-10 15:54:42 -0500

    Se agregó funcionalidad

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main  {
     public static void main(String[] arg){
-        System.out.println("Hello, JavaBank ATM");
+        System.out.println("Hello, JavaBank ATM Version control with Git");
     }
 }
